60a5b88d15aa1f16d74496de37748269de409293
Clara Arnold <claraarnold@u.boisestate.edu>
2023-12-04 18:49:48 -0700

    Working on persisting/saving selected date

--- components/Save.js
+++ components/Save.js
@@ -4,28 +4,28 @@ const filePath = FileSystem.documentDirectory + '/save_data';
 
 // dummy object
 const dummyObject = {
-  key: 1,
-  title: "Dentist Appt",
-  description: "Will be painful",
-  date: getDate(),
-  done: false,
-  selected: false,
-  location: {
-    lat: 0,
-    lng: 0,
-  },
+    key: 1,
+    title: "Dentist Appt",
+    description: "Will be painful",
+    date: getDate(),
+    done: false,
+    selected: false,
+    location: {
+        lat: 0,
+        lng: 0,
+    },
 };
 
 export async function load() {
-	let returnData=[dummyObject];
-	await FileSystem.readAsStringAsync(filePath).then((data) => {
-		returnData = JSON.parse(data);
-	}, (e) => {
-		console.log("An error occured in load(). This is expected when loading for the first time.",e);
-	});
-	return returnData;
+    let returnData=[dummyObject];
+    await FileSystem.readAsStringAsync(filePath).then((data) => {
+        returnData = JSON.parse(data);
+    }, (e) => {
+        console.log("An error occured in load(). This is expected when loading for the first time.",e);
+    });
+    return returnData;
 }
 
 export async function save(data) {
-	await FileSystem.writeAsStringAsync(filePath, JSON.stringify(data));
+    await FileSystem.writeAsStringAsync(filePath, JSON.stringify(data));
 }
